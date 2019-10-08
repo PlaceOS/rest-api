0317f2b179c85d68a25608b807df89364a43d6fb
Caspian Baska <caspianbaska@gmail.com>
2019-10-08 13:09:59 +1100

    feat(shard.yml config): add and configure service discovery

--- src/config.cr
+++ src/config.cr
@@ -19,6 +19,13 @@ require "action-controller/server"
 # Do not buffer logs
 STDOUT.sync = true
 
+# Configure Service discovery
+HoundDog.configure do |settings|
+  settings.logger = ActionController::Base.settings.logger
+  settings.etcd_host = ENV["ETCD_HOST"]? || "localhost"
+  settings.etcd_port = (ENV["ETCD_PORT"]? || 2379).to_i
+end
+
 # Add handlers that should run before your application
 ActionController::Server.before(
   HTTP::ErrorHandler.new(ENV["SG_ENV"]? != "production"),
