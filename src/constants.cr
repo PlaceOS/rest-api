5774c838c89ed3e0c7100a28e7fffbacdc6dba5b
Stephen von Takach <steve@acaprojects.com>
2020-02-16 13:21:42 +1100

    chore: crystal tool format

--- src/constants.cr
+++ src/constants.cr
@@ -1,6 +1,6 @@
 module ACAEngine::Api
-  APP_NAME       = "ACAEngine REST API"
-  API_VERSION    = "v2"
+  APP_NAME    = "ACAEngine REST API"
+  API_VERSION = "v2"
   # calculate version at compile time
   VERSION        = {{ system("shards version").stringify.strip.downcase }}
   CORE_NAMESPACE = "core"
