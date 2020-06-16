699c7121eacd43a492004826c205ddd7df32a80a
Caspian Baska <caspianbaska@gmail.com>
2020-06-16 16:49:41 +1000

    feat(controller:triggers): add instances route

--- spec/triggers_spec.cr
+++ spec/triggers_spec.cr
@@ -13,6 +13,25 @@ module PlaceOS::Api
         test_base_index(klass: Model::Trigger, controller_klass: Triggers)
       end
 
+      describe "/:id/instances" do
+        it "lists instances for a Trigger" do
+          trigger = Model::Generator.trigger.save!
+          instances = 3.times.to_a.map { Model::Generator.trigger_instance(trigger).save! }
+
+          response = curl(
+            method: "GET",
+            path: File.join(base, trigger.id.not_nil!, "instances"),
+            headers: authorization_header.merge({"Content-Type" => "application/json"}),
+          )
+
+          # Can't use from_json directly on the model as `id` will not be parsed
+          result = Array(JSON::Any).from_json(response.body).map { |d| Model::TriggerInstance.from_trusted_json(d.to_json) }
+
+          result.all? { |i| i.trigger_id == trigger.id }.should be_true
+          instances.compact_map(&.id).sort.should eq result.compact_map(&.id).sort
+        end
+      end
+
       describe "CRUD operations", tags: "crud" do
         test_crd(klass: Model::Trigger, controller_klass: Triggers)
         it "update" do
@@ -35,6 +54,26 @@ module PlaceOS::Api
           updated.id.should eq trigger.id
           updated.name.should_not eq original_name
           updated.destroy
+        end
+
+        describe "show" do
+          it "includes trigger_instances with truthy `instances`" do
+            trigger = Model::Generator.trigger.save!
+            trigger_instance = Model::Generator.trigger_instance(trigger).save!
+            trigger_instance_id = trigger_instance.id.as(String)
+
+            params = HTTP::Params{"instances" => "true"}
+            path = "#{base}#{trigger.id}?#{params}"
+
+            result = curl(
+              method: "GET",
+              path: path,
+              headers: authorization_header.merge({"Content-Type" => "application/json"}),
+            )
+
+            response = JSON.parse(result.body)
+            response["trigger_instances"].as_a?.try &.first?.try &.["id"].to_s.should eq trigger_instance_id
+          end
         end
       end
     end
