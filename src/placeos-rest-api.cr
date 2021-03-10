fe7d06cb3892b70fe6f982b88d2ab0c041fbd745
Caspian Baska <caspianbaska@gmail.com>
2021-03-10 12:40:29 +1100

    refactor(constants): move constants to a seperate file

--- src/placeos-rest-api.cr
+++ src/placeos-rest-api.cr
@@ -3,27 +3,12 @@ require "log_helper"
 require "placeos-log-backend"
 require "secrets-env"
 
+require "./constants"
 require "./placeos-rest-api/controllers/application"
 require "./placeos-rest-api/controllers/*"
 
 module PlaceOS::Api
-  APP_NAME    = "rest-api"
-  API_VERSION = "v2"
-  # Calculate version, build time, commit at compile time
-  VERSION        = {{ system(%(shards version "#{__DIR__}")).chomp.stringify.downcase }}
-  BUILD_TIME     = {{ system("date -u").stringify }}
-  BUILD_COMMIT   = {{ env("PLACE_COMMIT") || "DEV" }}
-  CORE_NAMESPACE = "core"
-
   Log = ::Log.for(APP_NAME)
-
-  ETCD_HOST = ENV["ETCD_HOST"]? || "localhost"
-  ETCD_PORT = (ENV["ETCD_PORT"]? || "2379").to_i
-
-  # server defaults in `./app.cr`
-  TRIGGERS_URI = URI.parse(ENV["TRIGGERS_URI"]? || "http://triggers:3000")
-
-  PROD = ENV["SG_ENV"]? == "production"
 
   class_getter? production : Bool = PROD
 end
