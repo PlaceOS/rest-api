53cf27c4cd469dc480a176c1b7ab8bb3cc99a779
Caspian Baska <caspianbaska@gmail.com>
2021-03-10 12:40:29 +1100

    refactor(constants): move constants to a seperate file

---
+++ src/constants.cr
@@ -0,0 +1,19 @@
+module PlaceOS::Api
+  APP_NAME    = "rest-api"
+  API_VERSION = "v2"
+
+  # Calculate version, build time, commit at compile time
+  VERSION      = {{ system(%(shards version "#{__DIR__}")).chomp.stringify.downcase }}
+  BUILD_TIME   = {{ system("date -u").stringify }}
+  BUILD_COMMIT = {{ env("PLACE_COMMIT") || "DEV" }}
+
+  CORE_NAMESPACE = "core"
+
+  ETCD_HOST = ENV["ETCD_HOST"]? || "localhost"
+  ETCD_PORT = (ENV["ETCD_PORT"]? || "2379").to_i
+
+  # server defaults in `./app.cr`
+  TRIGGERS_URI = URI.parse(ENV["TRIGGERS_URI"]? || "http://triggers:3000")
+
+  PROD = ENV["SG_ENV"]?.try(&.downcase) == "production"
+end
