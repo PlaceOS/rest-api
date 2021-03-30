4f133e2a732fa3272bd9d06e65be7851438247fb
Caspian Baska <caspianbaska@gmail.com>
2021-03-30 17:08:49 +1100

    feat(root): add a healthcheck that ensures the presence of etcd/rethinkdb/redis

--- src/placeos-rest-api/controllers/root.cr
+++ src/placeos-rest-api/controllers/root.cr
@@ -1,5 +1,8 @@
 require "./application"
+require "../utilities/core_discovery"
 
+require "rethinkdb"
+require "rethinkdb-orm"
 require "rubber-soul/client"
 
 module PlaceOS::Api
@@ -10,7 +13,45 @@ module PlaceOS::Api
     skip_action :check_oauth_scope, only: :signal
 
     get "/", :root do
-      head :ok
+      head self.class.healthcheck? ? HTTP::Status::OK : HTTP::Status::INTERNAL_SERVER_ERROR
+    end
+
+    def self.healthcheck? : Bool
+      Promise.all(
+        Promise.defer {
+          check_resource?("redis") { ::PlaceOS::Driver::RedisStorage.with_redis &.ping }
+        },
+        Promise.defer {
+          check_resource?("etcd") { Discovery::Core.instance.etcd &.maintenance.status }
+        },
+        Promise.defer {
+          check_resource?("rethinkdb") { rethinkdb_healthcheck }
+        },
+      ).then(&.all?).get
+    end
+
+    private def self.check_resource?(resource)
+      Log.trace { "healthchecking #{resource}" }
+      !!yield
+    rescue e
+      Log.error(exception: e) { {"connection check to #{resource} failed"} }
+      false
+    end
+
+    private def self.rethinkdb_healthcheck
+      connection = RethinkDB.connect(
+        host: RethinkORM.settings.host,
+        port: RethinkORM.settings.port,
+        db: "rethinkdb",
+        user: RethinkORM.settings.user,
+        password: RethinkORM.settings.password,
+        max_retry_attempts: 1,
+      )
+      RethinkDB
+        .table("server_status")
+        .pluck("id", "name")
+        .run(connection)
+        .first? ensure connection.close
     end
 
     get "/version", :version do
