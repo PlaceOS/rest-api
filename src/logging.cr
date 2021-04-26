040c2e9497764ff4767b2603c9f44e71f32dd43c
Caspian Baska <caspianbaska@gmail.com>
2021-04-26 15:04:35 +1000

    feat(logging): register log level change signals

--- src/logging.cr
+++ src/logging.cr
@@ -2,15 +2,25 @@ require "placeos-log-backend"
 
 module PlaceOS::Api
   # Logging configuration
-  log_level = Api.production? ? ::Log::Severity::Info : ::Log::Severity::Debug
   log_backend = PlaceOS::LogBackend.log_backend
+  log_level = Api.production? ? ::Log::Severity::Info : ::Log::Severity::Debug
+
   ::Log.setup "*", :warn, log_backend
-  ::Log.builder.bind "action-controller.*", log_level, log_backend
-  ::Log.builder.bind "place_os.*", log_level, log_backend
+
+  namespaces = ["action-controller.*", "place_os.*"]
+  namespaces.each do |namespace|
+    ::Log.builder.bind(namespace, log_level, log_backend)
+  end
 
   # Extra verbose coordination logging
   if ENV["PLACE_VERBOSE_CLUSTERING"]?.presence
     ::Log.builder.bind "hound_dog.*", ::Log::Severity::Debug, log_backend
     ::Log.builder.bind "clustering.*", ::Log::Severity::Debug, log_backend
   end
+
+  PlaceOS::LogBackend.register_severity_switch_signals(
+    production: Api.production?,
+    namespaces: namespaces,
+    backend: log_backend,
+  )
 end
