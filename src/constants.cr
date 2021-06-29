388f359978b889eacdf97e67c4103a29814515ef
tassja <xtassja@gmail.com>
2021-06-29 16:13:15 +1000

    test: update service version spec

--- src/constants.cr
+++ src/constants.cr
@@ -12,6 +12,8 @@ module PlaceOS::Api
   ETCD_HOST = ENV["ETCD_HOST"]? || "localhost"
   ETCD_PORT = (ENV["ETCD_PORT"]? || "2379").to_i
 
+  PLACE_URI = URI.parse(ENV["PLACE_URI"]? || "http://127.0.0.1:3000")
+
   # server defaults in `./app.cr`
   TRIGGERS_URI = URI.parse(ENV["TRIGGERS_URI"]? || "http://triggers:3000")
 
