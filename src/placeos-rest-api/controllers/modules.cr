53b475251b32aa01a039a1bc62e9311d6c68ceab
Caspian Baska <email@caspian.computer>
2021-09-22 14:01:54 +1000

    fix(api/modules): remove parent index query

--- src/placeos-rest-api/controllers/modules.cr
+++ src/placeos-rest-api/controllers/modules.cr
@@ -98,7 +98,9 @@ module PlaceOS::Api
           query.must_not({"role" => [Model::Driver::Role::Logic.to_i]})
         end
 
-        query.has_parent(parent: Model::Driver, parent_index: Model::Driver.table_name)
+        # NOTE: parent queries appear to fail as of `placeos-1.2109.1`
+        # query.has_parent(parent: Model::Driver, parent_index: Model::Driver.table_name)
+
         search_results = paginate_results(elastic, query)
 
         # Include subset of association data with results
