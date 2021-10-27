310f62435e43c3f01c160a281d4e3cb48793d6a7
tassja <xtassja@gmail.com>
2021-10-27 15:36:28 +1100

    style: ameba check

--- spec/spec_constants.cr
+++ spec/spec_constants.cr
@@ -2,6 +2,7 @@ require "./helper"
 
 module PlaceOS::Api
   class_getter authorization_header : Hash(String, String) do
+    # ameba:disable Lint/UselessAssign
     _, header = authentication
   end
 end
