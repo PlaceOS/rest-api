ef5a8cba21b5eb52ee0dd9d38d1bca158b1fe10c
tassja <xtassja@gmail.com>
2021-12-21 14:22:54 +1100

    test(mqtt): add spec

--- spec/controllers/mqtt_spec.cr
+++ spec/controllers/mqtt_spec.cr
@@ -23,9 +23,27 @@ module PlaceOS::Api
               end
           end
 
-          pending "allows #{MQTT::MqttAcl::Read} access"
-          pending "allows #{MQTT::MqttAcl::Write} access for support and above"
-          pending "denies #{MQTT::MqttAcl::Write} access"
+          it "allows #{MQTT::MqttAcl::Read} access" do
+            scope = [PlaceOS::Model::UserJWT::Scope.new("mqtt", :read)]
+            authenticated_user, _scoped_authorization_header = authentication(scope: scope)
+            user_jwt = PlaceOS::Model::Generator.jwt(authenticated_user, scope)
+            MQTT.mqtt_acl_status(MQTT::MqttAcl::Read, user_jwt).should eq HTTP::Status::OK
+          end
+
+           
+          it "allows #{MQTT::MqttAcl::Write} access for support and above" do
+            scope = [PlaceOS::Model::UserJWT::Scope.new("mqtt", :write)]
+            authenticated_user, _scoped_authorization_header = authentication(scope: scope)
+            user_jwt = PlaceOS::Model::Generator.jwt(authenticated_user, scope)
+            MQTT.mqtt_acl_status(MQTT::MqttAcl::Write, user_jwt).should eq HTTP::Status::OK
+          end
+
+          it "denies #{MQTT::MqttAcl::Write} access for under support" do
+            authenticated_user, _ = authentication(sys_admin: false, support: false, scope: scope)
+            user_jwt = PlaceOS::Model::Generator.jwt(authenticated_user, scope)
+            MQTT.mqtt_acl_status(MQTT::MqttAcl::Write, user_jwt).should eq HTTP::Status::FORBIDDEN
+
+          end
         end
       end
     end
