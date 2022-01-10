f8beaf14146ceb4e3ae1b6ae65f2c37817a49e53
tassja <xtassja@gmail.com>
2022-01-10 12:59:50 +1100

    test: update spec

--- spec/controllers/settings_spec.cr
+++ spec/controllers/settings_spec.cr
@@ -43,26 +43,23 @@ module PlaceOS::Api
 
       describe "index", tags: "search" do
         pending "searchs on keys" do
-        sys = Model::Generator.control_system.save!
-        settings = [
-          Model::Generator.settings(encryption_level: Encryption::Level::None, control_system: sys),
-          Model::Generator.settings(encryption_level: Encryption::Level::Admin, control_system: sys),
-          Model::Generator.settings(encryption_level: Encryption::Level::NeverDisplay, control_system: sys),
-        ]
-        clear, admin, never_displayed = settings.map(&.save!)
-        refresh_elastic(Model::Settings.table_name)
+          sys = Model::Generator.control_system.save!
+          settings = [
+            Model::Generator.settings(encryption_level: Encryption::Level::None, control_system: sys),
+            Model::Generator.settings(encryption_level: Encryption::Level::Admin, control_system: sys),
+            Model::Generator.settings(encryption_level: Encryption::Level::NeverDisplay, control_system: sys),
+          ]
+          clear, admin, never_displayed = settings.map(&.save!)
+          refresh_elastic(Model::Settings.table_name)
 
+          params = HTTP::Params.encode({"q" => Encryption::Level::NeverDisplay.to_json})
+          path = "#{base.rstrip('/')}?#{params}"
 
-        params = HTTP::Params.encode({"q" => Encryption::Level::NeverDisplay.to_json})
-        path = "#{base.rstrip('/')}?#{params}"
-
-
-        result = curl(
-          method: "GET",
-          path: path,
-          headers: authorization_header
-        )
-
+          result = curl(
+            method: "GET",
+            path: path,
+            headers: authorization_header
+          )
         end
 
         it "returns settings for a set of parent ids" do
@@ -93,18 +90,18 @@ module PlaceOS::Api
 
           returned_settings = Array(JSON::Any).from_json(result.body).map { |m|
             Model::Settings.from_trusted_json(m.to_json)
-        }
+          }
 
-        returned_settings.size.should eq(6)
+          returned_settings.size.should eq(6)
 
-        sys1_never_displayed, sys2_never_displayed = returned_settings[0..1]
-        (sys1_never_displayed.encryption_level == Encryption::Level::NeverDisplay && sys2_never_displayed.encryption_level == Encryption::Level::NeverDisplay).should be_true
+          sys1_never_displayed, sys2_never_displayed = returned_settings[0..1]
+          (sys1_never_displayed.encryption_level == Encryption::Level::NeverDisplay && sys2_never_displayed.encryption_level == Encryption::Level::NeverDisplay).should be_true
 
-        sys1_admin, sys2_admin = returned_settings[2..3]
-        (sys1_admin.encryption_level == Encryption::Level::Admin && sys2_admin.encryption_level == Encryption::Level::Admin).should be_true
+          sys1_admin, sys2_admin = returned_settings[2..3]
+          (sys1_admin.encryption_level == Encryption::Level::Admin && sys2_admin.encryption_level == Encryption::Level::Admin).should be_true
 
-        sys1_clear, sys2_clear = returned_settings[4..5]
-        (sys1_clear.encryption_level == Encryption::Level::None  && sys2_clear.encryption_level == Encryption::Level::None).should be_true
+          sys1_clear, sys2_clear = returned_settings[4..5]
+          (sys1_clear.encryption_level == Encryption::Level::None && sys2_clear.encryption_level == Encryption::Level::None).should be_true
         end
 
         it "returns settings for parent id" do
