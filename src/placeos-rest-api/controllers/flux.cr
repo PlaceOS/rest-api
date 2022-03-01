5c0c3396474afde1f7785bc7f43f6815cd918c72
Stephen von Takach <steve@vontaka.ch>
2022-03-01 13:36:27 +1100

    move constants

--- src/placeos-rest-api/controllers/flux.cr
+++ src/placeos-rest-api/controllers/flux.cr
@@ -9,23 +9,24 @@ module PlaceOS::Api
 
     post("/query", :query) do
       request_headers = request.headers
-      proxy_headers = HTTP::Headers.new
-      proxy_headers.add "Accept", request_headers["Accept"]? || "application/csv"
-      proxy_headers.add "Content-Type", request_headers["Content-Type"]? || "application/vnd.flux"
 
       # stream the data
-      flux_client.post "/query", proxy_headers, body do |result|
+      flux_client.post "/query", HTTP::Headers{
+        "Accept"       => request_headers["Accept"]? || "application/csv",
+        "Content-Type" => request_headers["Content-Type"]? || "application/vnd.flux",
+      }, body do |result|
         response.status_code = result.status_code
         response.headers["Content-Type"] = result.headers["Content-Type"]
         IO.copy(result.body_io, response)
       end
+
       @render_called = true
     end
 
     getter flux_client : HTTP::Client do
-      api_key = ENV["INFLUX_API_KEY"]
-      org = ENV["INFLUX_ORG"]? || "placeos"
-      connection = HTTP::Client.new URI.parse(ENV["INFLUX_HOST"])
+      api_key = INFLUX_API_KEY || raise("no Influx API key configured")
+      org = INFLUX_ORG
+      connection = HTTP::Client.new URI.parse(INFLUX_HOST || raise("no Influx hostname configured"))
       connection.before_request do |req|
         req.headers["Authorization"] = "Token #{api_key}"
         req.path = "/api/v2#{req.path}"
