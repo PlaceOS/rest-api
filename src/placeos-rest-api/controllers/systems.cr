0adf317efc3066c6348b0a784fa58e5cdea3f63c
Stephen von Takach <steve@vontaka.ch>
2022-03-02 23:23:49 +1100

    fix(systems execute): fix response using driver status codes (#240)
    * fix(systems execute): fix response using driver status codes
    
    * refactor(systems): destructure
    
    Co-authored-by: Caspian Baska <email@caspian.computer>

--- src/placeos-rest-api/controllers/systems.cr
+++ src/placeos-rest-api/controllers/systems.cr
@@ -325,14 +325,14 @@ module PlaceOS::Api
         user_id: current_user.id,
       )
 
-      ret_val = remote_driver.exec(
+      response_text, status_code = remote_driver.exec(
         security: driver_clearance(user_token),
         function: method,
         args: args,
         request_id: request_id,
       )
       response.headers["Content-Type"] = "application/json"
-      render text: ret_val
+      render text: response_text, status: status_code
     rescue e : RemoteDriver::Error
       handle_execute_error(e)
     end
