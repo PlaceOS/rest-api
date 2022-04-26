f78e3b4482860c7123b4feb6413ffa2fb0f26ec3
Caspian Baska <email@caspian.computer>
2022-04-26 21:34:22 +1000

    feat(logging): add configuration by LOG_LEVEL env var

--- src/logging.cr
+++ src/logging.cr
@@ -16,7 +16,7 @@ module PlaceOS::Api::Logging
   namespaces = ["action-controller.*", "place_os.*"]
 
   builder = ::Log.builder
-  builder.bind("*", :warn, log_backend)
+  builder.bind("*", log_level, log_backend)
   namespaces.each do |namespace|
     builder.bind(namespace, log_level, log_backend)
 
