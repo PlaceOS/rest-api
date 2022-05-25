3a28f1ca85f12b24df6b464739c26c23158b4e50
camreeves <62260424+camreeves@users.noreply.github.com>
2022-05-25 14:15:38 +1000

    test(edge): authentication of edge client with the API (#278)
    * fix(edges): use string for edge control scope as constant not present before runtime
    
    * test(edge): add spec for edge authentication
    
    * ci(docker-compose): use alpine
    
    * ci(docker-compose): default to crystal 1.4.1
    
    * build(shard.yml): add placeos-core development dependency
    
    * test(edges): use `new` over json blob
    
    * chore: bump core
    
    * style: tiny change
    
    Co-authored-by: Cameron Reeves <cam@place.tech>
    Co-authored-by: Caspian Baska <email@caspian.computer>

--- spec/controllers/edges_spec.cr
+++ spec/controllers/edges_spec.cr
@@ -1,8 +1,9 @@
 require "../helper"
+require "placeos-core/placeos-edge/client"
 
 module PlaceOS::Api
   describe Edges do
-    _authenticated_user, authorization_header = authentication
+    authenticated_user, authorization_header = authentication
     base = Edges::NAMESPACE[0]
 
     with_server do
@@ -10,6 +11,37 @@ module PlaceOS::Api
 
       describe "index", tags: "search" do
         Specs.test_base_index(Model::Edge, Edges)
+      end
+
+      describe "/control" do
+        it "authenticates with an API key from a new edge" do
+          # Create a new edge to test with as the controller would
+          edge_name = "Test Edge"
+          edge_host = "localhost"
+          edge_port = 6000
+
+          create_body = Model::Edge::CreateBody.new(name: edge_name, user_id: authenticated_user.id.as(String))
+          new_edge = Model::Edge.for_user(
+            user: authenticated_user,
+            name: create_body.name,
+            description: create_body.description
+          )
+
+          # Ensure instance variable initialised and edge saved
+          new_edge.x_api_key
+          new_edge.save!
+
+          uri = URI.new(host: edge_host, port: edge_port, query: "api-key=#{new_edge.x_api_key}")
+          client = PlaceOS::Edge::Client.new(
+            uri: uri,
+            secret: new_edge.x_api_key
+          )
+
+          client.connect do
+            client.transport.closed?.should_not be_nil
+            client.disconnect
+          end
+        end
       end
 
       describe "CRUD operations", tags: "crud" do
