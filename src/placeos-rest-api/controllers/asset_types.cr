65d83129a92f1abd0ff28053382cb6c163eca302
Stephen von Takach <steve@place.technology>
2023-06-29 13:11:24 +1000

    fix query

--- src/placeos-rest-api/controllers/asset_types.cr
+++ src/placeos-rest-api/controllers/asset_types.cr
@@ -37,10 +37,11 @@ module PlaceOS::Api
       }
 
       PgORM::Database.connection do |db|
-        db.query_all(
-          sql_query,
-          as: NamedTuple(asset_type_id: String, child_count: Int64)
-        ).each { |count| counts[count[:asset_type_id]] = count[:child_count] }
+        db.query(sql_query) do |rs|
+          asset_type_id = rs.read(String)
+          count = rs.read(Int64)
+          counts[asset_type_id] = count
+        end
       end
 
       results.each { |type| type.asset_count = counts[type.id]? || 0_i64 }
