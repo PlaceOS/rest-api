f79385ba1f6836ddc07abc653155c54d6266de8d
Stephen von Takach <steve@place.technology>
2026-09-09 11:03:30 +1000

    feat(signage): change param that saves analytics

--- src/placeos-rest-api/controllers/signage.cr
+++ src/placeos-rest-api/controllers/signage.cr
@@ -26,7 +26,7 @@ module PlaceOS::Api
       @[AC::Param::Info(description: "currently playing item, if the player is playing content", example: "playlist_items-1234")]
       item_id : String? = nil,
       @[AC::Param::Info(description: "is this the preview player", example: "true")]
-      preview : Bool? = nil,
+      preview : Bool = false,
     ) : ::PlaceOS::Model::ControlSystem?
       # grab all the playlists and templates associated with the display and
       # fingerprint them to check if anything has changed
@@ -37,7 +37,7 @@ module PlaceOS::Api
 
       # on the player, `?debug=` sets preview - considered production if this
       # is not configured at all.
-      if preview.nil?
+      if !preview
         # Save last seen and currently playing item
         item_id = item_id.presence
         if item_id
